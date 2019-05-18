34f0a2321bd04ba09ca595fe8e501a7c0b3a7819
Dmitry Yudakov <t-d-k@yandex.ru>
2019-05-19 00:36:21 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimilarTagsCalculator {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+
+    class SimilarTagsCalculator {
+        const int TagGroupsCount = 1000000;
+        const int TagsPerGroup = 4096;
+
+
+    }
+}
